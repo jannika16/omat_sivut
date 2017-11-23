972e11d57c7887f449768a93863a83e5cbc3a391
jannika <31062722+jannika16@users.noreply.github.com>
2017-11-23 13:08:38 +0200

    Update tallennaProjekti.php

--- tallennaProjekti.php
+++ tallennaProjekti.php
@@ -1,17 +1,19 @@
 <?php
-if (empty($GET)) {
-  die("...");
+if (empty($_GET)) {
+  die("Ei toimi!!!");
 }
 
   $nimi = $_GET['nimi'];
   $pvm = $_GET['pvm'];
   $kuvaus = $_GET['kuvaus'];
+  // $kuva = $_GET['kuva'];
 
   $errors = array();
 
   if (empty($nimi)) $errors[] = 'Anna nimi!';
   if (empty($pvm)) $errors[] = 'Anna päivämäärä!';
   if (empty($kuvaus)) $errors[] = 'Anna kuvaus!';
+  // if (empty($kuva)) $errors[] = 'Anna kuva!';
 
   if (!empty($errors)) {
     $output = '<ul><li>' . implode('</li><li>',$errors) . '</li></ul>';
@@ -19,16 +21,19 @@ if (empty($GET)) {
     $output = '...';
 
     $xml = simplexml_load_file('data/projekti.xml');
-    $uusiprojetki = $xml->addChild('projekti');
+    $uusiprojekti = $xml->addChild('projekti');
     $uusiprojekti->addChild('nimi', $nimi);
     $uusiprojekti->addChild('pvm', $pvm);
     $uusiprojekti->addChild('kuvaus', $kuvaus);
+    // $uusiprojekti->addChild('kuva', $kuva);
 
     $dom = new DOMDocument("1.0");
     $dom->preserveWhiteSpace = false;
     $dom->formatOutput = true;
     $dom->loadXML($xml->asXML());
     $dom->save('data/projekti.xml');
+
+
   }
 
   header('refresh:2;url=projektit.html');
